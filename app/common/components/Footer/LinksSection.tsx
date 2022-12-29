64a198f229fb22e7feb6643e611f92bd7bb4f447
dbrosio3 <daniel.brosio@rootstrap.com>
2022-12-29 16:07:04 -0300

    add link underline

--- app/common/components/Footer/LinksSection.tsx
+++ app/common/components/Footer/LinksSection.tsx
@@ -3,49 +3,49 @@ export const LinksSection = () => {
     <div className="mb-8 flex flex-col justify-between md:flex-row">
       <div className="flex flex-col">
         <h5 className="text-base font-bold text-white">Restored furniture</h5>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Entries
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Rates
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Categories
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Sale
         </a>
       </div>
       <div className="flex flex-col">
         <h5 className="text-base font-bold text-white">Stay connected</h5>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Instagram
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Tik Tok
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Facebook
         </a>
       </div>
       <div className="flex flex-col">
         <h5 className="text-base font-bold text-white">Black Market</h5>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Our history
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Staff
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Work with us
         </a>
       </div>
       <div className="flex flex-col">
         <h5 className="text-base font-bold text-white">Support</h5>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Chat
         </a>
-        <a href="/" className="mt-4 text-base text-white">
+        <a href="/" className="mt-4 text-base text-white hover:underline">
           Address
         </a>
       </div>

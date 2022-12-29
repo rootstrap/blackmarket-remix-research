9584d4bc65e301bc101a61e45348aa5f7e7dafaf
dbrosio3 <daniel.brosio@rootstrap.com>
2022-12-29 16:00:45 -0300

    splitted footer into separate components

---
+++ app/common/components/Footer/LinksSection.tsx
@@ -0,0 +1,54 @@
+export const LinksSection = () => {
+  return (
+    <div className="mb-8 flex flex-col justify-between md:flex-row">
+      <div className="flex flex-col">
+        <h5 className="text-base font-bold text-white">Restored furniture</h5>
+        <a href="/" className="mt-4 text-base text-white">
+          Entries
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Rates
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Categories
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Sale
+        </a>
+      </div>
+      <div className="flex flex-col">
+        <h5 className="text-base font-bold text-white">Stay connected</h5>
+        <a href="/" className="mt-4 text-base text-white">
+          Instagram
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Tik Tok
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Facebook
+        </a>
+      </div>
+      <div className="flex flex-col">
+        <h5 className="text-base font-bold text-white">Black Market</h5>
+        <a href="/" className="mt-4 text-base text-white">
+          Our history
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Staff
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Work with us
+        </a>
+      </div>
+      <div className="flex flex-col">
+        <h5 className="text-base font-bold text-white">Support</h5>
+        <a href="/" className="mt-4 text-base text-white">
+          Chat
+        </a>
+        <a href="/" className="mt-4 text-base text-white">
+          Address
+        </a>
+      </div>
+    </div>
+  );
+};
